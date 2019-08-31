eb45caf4854b8edb84c9aebb012ddfd8c457dd08
RenanMDP <renanmartineli@hotmail.com>
2019-08-31 20:05:41 -0300

    score implementation

--- rps-app.js
+++ rps-app.js
@@ -1,103 +1,121 @@
-// hand choice functions
-rock.onclick = function() {
-  const rock = document.querySelector("#rock");
-  let userInput;
-  userInput = rock.getAttribute("id");
+(function(){
+  // score variables
+  let playerScoreCount = 0;
+  let computerScoreCount = 0;
 
-  const userChoice = userInput;
-  const computerChoice = getComputerChoice();
+  // hand choice functions
+  rock.onclick = function() {
+    const rock = document.querySelector("#rock");
+    let userInput;
+    userInput = rock.getAttribute("id");
 
-  document.querySelector("#compChoice").textContent = `Computer picked: ${computerChoice}`;
-  document.querySelector("#outcome").textContent = `${determineWinner(userChoice, computerChoice)}`;
-}
+    const userChoice = userInput;
+    const computerChoice = getComputerChoice();
 
-paper.onclick = function() {
-  const paper = document.querySelector("#paper");
-  let userInput;
-  userInput = paper.getAttribute("id");
-
-  const userChoice = userInput;
-  const computerChoice = getComputerChoice();
-
-  document.querySelector("#compChoice").textContent = `Computer picked: ${computerChoice}`;
-  document.querySelector("#outcome").textContent = `${determineWinner(userChoice, computerChoice)}`;
-}
-
-scissors.onclick = function() {
-  const scissors = document.querySelector("#scissors");
-  let userInput;
-  userInput = scissors.getAttribute("id");
-
-  const userChoice = userInput;
-  const computerChoice = getComputerChoice();
-
-  document.querySelector("#compChoice").textContent = `Computer picked: ${computerChoice}`;
-  document.querySelector("#outcome").textContent = `${determineWinner(userChoice, computerChoice)}`;
-}
-
-// gets the computer choice
-const getComputerChoice = () => {
-  const compInput = Math.floor(Math.random() * 3);
-  
-  switch(compInput) {
-    case 0:
-      return 'rock';
-      break;
-    case 1:
-      return 'paper';
-      break;
-    case 2:
-      return 'scissors';
-      break;
+    document.querySelector("#compChoice").textContent = `Computer picked: ${computerChoice}`;
+    document.querySelector("#outcome").textContent = `${determineWinner(userChoice, computerChoice)}`;
   }
-};
 
-// decides the outcome based on user and computer choices
-const determineWinner = (userChoice, computerChoice) => {
-  const playSound = {
-    audio: document.querySelector("audio"),
-    winSound: "sound/kids-shouting-yay-sound-effect.mp3",
-    loseSound: "sound/aww-sound-effect.mp3",
-    tieSound: "sound/roblox-death-sound.mp3",
-  
-    playerWin() {
-      this.audio.src = this.winSound;
-      this.audio.play();
-      return `Player wins! :D`;
-    },
-  
-    playerLose() {
-      this.audio.src = this.loseSound;
-      this.audio.play();
-      return `Computer wins! :(`;
-    },
-  
-    playerTie() {
-      this.audio.src = this.tieSound;
-      this.audio.play();
-      return `It's a tie! :/`;
+  paper.onclick = function() {
+    const paper = document.querySelector("#paper");
+    let userInput;
+    userInput = paper.getAttribute("id");
+
+    const userChoice = userInput;
+    const computerChoice = getComputerChoice();
+
+    document.querySelector("#compChoice").textContent = `Computer picked: ${computerChoice}`;
+    document.querySelector("#outcome").textContent = `${determineWinner(userChoice, computerChoice)}`;
+  }
+
+  scissors.onclick = function() {
+    const scissors = document.querySelector("#scissors");
+    let userInput;
+    userInput = scissors.getAttribute("id");
+
+    const userChoice = userInput;
+    const computerChoice = getComputerChoice();
+
+    document.querySelector("#compChoice").textContent = `Computer picked: ${computerChoice}`;
+    document.querySelector("#outcome").textContent = `${determineWinner(userChoice, computerChoice)}`;
+  }
+
+  // gets the computer choice
+  const getComputerChoice = () => {
+    const compInput = Math.floor(Math.random() * 3);
+    
+    switch(compInput) {
+      case 0:
+        return 'rock';
+        break;
+      case 1:
+        return 'paper';
+        break;
+      case 2:
+        return 'scissors';
+        break;
     }
   };
 
-  if ((userChoice === computerChoice)) {
-    return playSound.playerTie()
-  } else if ((userChoice === 'rock')) {
-      if ((computerChoice === 'paper')) {
-        return playSound.playerLose();
-      } else {
-        return playSound.playerWin();
-      }
-  } else if ((userChoice === 'paper')) {
-    	if ((computerChoice === 'scissors')) {
-        return playSound.playerLose();
-      } else {
-        return playSound.playerWin();
-      }
-  } else if ((userChoice === 'scissors')) {
-      if ((computerChoice === 'rock')) {
-        return playSound.playerLose();
-      } else {
-        return playSound.playerWin();
-      }
-  }
-};
+  // decides the outcome based on user and computer choices
+  const determineWinner = (userChoice, computerChoice) => {
+    // keep game score
+    const playerScoreTag = document.querySelector("#playerScore");
+    const computerScoreTag = document.querySelector("#computerScore");
+    playerScoreTag.textContent = `Player: ${playerScoreCount}`;
+    computerScoreTag.textContent = `Computer: ${computerScoreCount}`;
+    
+    // play sound according to each outcome
+    const playSound = {
+      audio: document.querySelector("audio"),
+      winSound: "sound/kids-shouting-yay-sound-effect.mp3",
+      loseSound: "sound/aww-sound-effect.mp3",
+      tieSound: "sound/roblox-death-sound.mp3",
+
+    
+      playerWin() {
+        playerScoreCount++;
+        playerScoreTag.textContent = `Player: ${playerScoreCount}`;
+        this.audio.src = this.winSound;
+        this.audio.play();
+        return `Player wins! :D`;
+      },
+    
+      playerLose() {
+        computerScoreCount++;
+        computerScoreTag.textContent = `Computer: ${computerScoreCount}`;
+        this.audio.src = this.loseSound;
+        this.audio.play();
+        return `Computer wins! :(`;
+      },
+    
+      playerTie() {
+        this.audio.src = this.tieSound;
+        this.audio.play();
+        return `It's a tie! :/`;
+      },
+    };
+
+    if ((userChoice === computerChoice)) {
+      return playSound.playerTie();
+    } else if ((userChoice === 'rock')) {
+        if ((computerChoice === 'paper')) {
+          return playSound.playerLose();
+        } else {
+          return playSound.playerWin();
+        }
+    } else if ((userChoice === 'paper')) {
+        if ((computerChoice === 'scissors')) {
+          return playSound.playerLose();
+        } else {
+          return playSound.playerWin();
+        }
+    } else if ((userChoice === 'scissors')) {
+        if ((computerChoice === 'rock')) {
+          return playSound.playerLose();
+        } else {
+          return playSound.playerWin();
+        }
+    }
+  };
+})();
